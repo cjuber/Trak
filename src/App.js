b1e63d76fb80490f9679ef894e5b1a2db9298115
Chris Juber <cjuber5@gmail.com>
2021-11-04 19:33:01 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './css/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app'>
+      <header>
+        <div className='logoBox'>
+        <div className="logo"></div>
+        </div>
+        <div className='logIn'>
+        <div className='signIn'>Sign In</div>
+        <div className='signUp'>Sign Up</div>
+        </div>
       </header>
+      <div className='heroColor'>
+      <div className='hero'> 
+        <div className="heroTitle">TrakIt</div>
+        <div className='heroText'>Track and Find where your stuff is</div>
+      </div>
+      </div>
+      <div className='quoteBox'>
+        <div className='quote'>“I feel like I’ll never lose another thing in my home again. TrakIt helps to let me know where everything is.” Chris Juber</div>
+      </div>
+      <div className='linkBoxes'> 
+      <div className='linkBox1'>
+      <div className='linkBox'>
+        <div className="linkTitle">What is Trakit</div>
+        <div className="readMore">Read More</div>
+      </div>
+      </div>
+      <div className='linkBox2'>
+      <div className='linkBox'>
+        <div className="linkTitle">Plans</div>
+        <div className="readMore">Read More</div>
+      </div>
+      </div>
+      <div className='linkBox3'>
+      <div className='linkBox'>
+        <div className="linkTitle">Contact</div>
+        <div className="readMore">Read More</div>
+      </div>
+      </div>
+      </div>
+      
     </div>
   );
 }
